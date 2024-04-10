cefe3a866fc0050b26e63038b1415ac0a9017b10
Morten Winkler <morten@winkler.dk>
2024-04-10 17:06:46 +0200

    Add script to build and run tests in docker container.

--- test_in_docker.sh
+++ test_in_docker.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-docker build . -r webcrawler
+docker build . -t webcrawler
 docker run --rm  -v `pwd`:/work webcrawler cd /work ; rm -rf tb; mkdir tb; cd tb; cmake ../ ; make -j ; ctest
